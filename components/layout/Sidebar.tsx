1a7dd2498429894d851d7322f1cee98788513da8
Radu Lefter <lefter_radu@yahoo.com>
2023-04-18 22:31:13 +0100

    created sidebar item component and added it to the sidebar

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,5 +1,7 @@
 import { BsHouseFill, BsBellFill } from 'react-icons/bs';
+import { BiLogOut } from 'react-icons/bi';
 import { FaUser } from 'react-icons/fa';
+import SidebarItem from './SidebarItem';
 import SidebarLogo from './SidebarLogo';
 
 const Sidebar = () => {
@@ -28,7 +30,15 @@ const Sidebar = () => {
       <div className="flex flex-col items-end">
         <div className="space-y-2 lg:w-[230px]">
           <SidebarLogo />
-          <div>Sidebar</div>;
+          {items.map((item) => (
+              <SidebarItem
+                key={item.href}
+                href={item.href} 
+                icon={item.icon} 
+                label={item.label}
+              />
+            ))}
+            <SidebarItem onClick={() => {}} icon={BiLogOut} label="Logout" />
         </div>
       </div>
     </div>
