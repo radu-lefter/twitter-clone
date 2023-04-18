55d926605b8233e1dc3854ce642997bb7b888e14
Radu Lefter <lefter_radu@yahoo.com>
2023-04-18 22:38:56 +0100

    created basic follow bar

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,5 @@
 import Sidebar from './layout/Sidebar';
+import FollowBar from "@/components/layout/FollowBar"
 
 interface LayoutProps {
   children: React.ReactNode;
@@ -20,6 +21,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
           >
             {children}
           </div>
+          <FollowBar />
         </div>
       </div>
     </div>
