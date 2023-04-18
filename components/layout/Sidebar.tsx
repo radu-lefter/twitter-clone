49f60b2c890207826561c96370fcf4cbae05c876
Radu Lefter <lefter_radu@yahoo.com>
2023-04-18 22:24:10 +0100

    created sidebar logo component

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,6 +1,6 @@
-
 import { BsHouseFill, BsBellFill } from 'react-icons/bs';
 import { FaUser } from 'react-icons/fa';
+import SidebarLogo from './SidebarLogo';
 
 const Sidebar = () => {
   const items = [
@@ -23,7 +23,16 @@ const Sidebar = () => {
     },
   ];
 
-  return <div>Sidebar</div>;
+  return (
+    <div className="col-span-1 h-full pr-4 md:pr-6">
+      <div className="flex flex-col items-end">
+        <div className="space-y-2 lg:w-[230px]">
+          <SidebarLogo />
+          <div>Sidebar</div>;
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
