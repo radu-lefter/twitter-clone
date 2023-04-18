4ee5f95b4c567edb95358585c0cec103c8cefcb7
Radu Lefter <lefter_radu@yahoo.com>
2023-04-18 22:21:20 +0100

    added icons array in sidebar

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,8 +1,29 @@
 
+import { BsHouseFill, BsBellFill } from 'react-icons/bs';
+import { FaUser } from 'react-icons/fa';
+
 const Sidebar = () => {
-    return(
-        <div>Sidebar</div>
-    )
-}
+  const items = [
+    {
+      icon: BsHouseFill,
+      label: 'Home',
+      href: '/',
+    },
+    {
+      icon: BsBellFill,
+      label: 'Notifications',
+      href: '/notifications',
+      auth: true,
+    },
+    {
+      icon: FaUser,
+      label: 'Profile',
+      href: `/users/123`,
+      auth: true,
+    },
+  ];
+
+  return <div>Sidebar</div>;
+};
 
 export default Sidebar;
