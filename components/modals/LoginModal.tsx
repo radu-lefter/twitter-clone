bc93cd65e06cdd1c004045f55a87c1dc46a079fe
raduubraduu <lefter_radu@yahoo.com>
2023-04-20 18:56:35 +0100

    modified login modal to login user

--- components/modals/LoginModal.tsx
+++ components/modals/LoginModal.tsx
@@ -1,4 +1,3 @@
-
 import { useCallback, useState } from "react";
 import useLoginModal from "@/hooks/useLoginModal";
 
@@ -6,66 +5,77 @@ import Input from "../Input";
 import Modal from "../Modal";
 import useRegisterModal from "@/hooks/useRegisterModal";
 
+import { signIn } from "next-auth/react";
+import { toast } from "react-hot-toast";
+
 const LoginModal = () => {
   const loginModal = useLoginModal();
   const registerModal = useRegisterModal();
 
-
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
   const onSubmit = useCallback(async () => {
     try {
       setIsLoading(true);
 
+      await signIn("credentials", {
+        email,
+        password,
+      });
+
+      toast.success("Logged in");
 
       loginModal.onClose();
     } catch (error) {
-console.log(error)
+      toast.error("Something went wrong");
     } finally {
       setIsLoading(false);
     }
-  }, [loginModal]);
+  }, [email, password, loginModal]);
 
   const onToggle = useCallback(() => {
     loginModal.onClose();
     registerModal.onOpen();
-  }, [loginModal, registerModal])
-
+  }, [loginModal, registerModal]);
 
   const bodyContent = (
     <div className="flex flex-col gap-4">
-      <Input 
+      <Input
         placeholder="Email"
         onChange={(e) => setEmail(e.target.value)}
         value={email}
-        disabled={isLoading}  
+        disabled={isLoading}
       />
-      <Input 
+      <Input
         placeholder="Password"
         type="password"
         onChange={(e) => setPassword(e.target.value)}
         value={password}
-        disabled={isLoading} 
+        disabled={isLoading}
       />
     </div>
-  )
+  );
 
   const footerContent = (
     <div className="text-neutral-400 text-center mt-4">
-      <p>First time using Twitter?
-        <span 
-          onClick={onToggle} 
+      <p>
+        First time using Twitter?
+        <span
+          onClick={onToggle}
           className="
             text-white 
             cursor-pointer 
             hover:underline
           "
-          > Create an account</span>
+        >
+          {" "}
+          Create an account
+        </span>
       </p>
     </div>
-  )
+  );
 
   return (
     <Modal
@@ -79,6 +89,6 @@ console.log(error)
       footer={footerContent}
     />
   );
-}
+};
 
 export default LoginModal;
