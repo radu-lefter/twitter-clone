287707751219c86a84cbf2735bf22a7736dadfe6
raduubraduu <lefter_radu@yahoo.com>
2023-04-24 22:06:45 +0100

    created useLike hook and implemented like functionality

--- components/posts/PostItem.tsx
+++ components/posts/PostItem.tsx
@@ -5,7 +5,7 @@ import { formatDistanceToNowStrict } from 'date-fns';
 
 import useLoginModal from '@/hooks/useLoginModal';
 import useCurrentUser from '@/hooks/useCurrentUser';
-//import useLike from '@/hooks/useLike';
+import useLike from '@/hooks/useLike';
 
 import Avatar from '../Avatar';
 interface PostItemProps {
@@ -18,7 +18,7 @@ const PostItem: React.FC<PostItemProps> = ({ data = {}, userId }) => {
   const loginModal = useLoginModal();
 
   const { data: currentUser } = useCurrentUser();
-  //const { hasLiked, toggleLike } = useLike({ postId: data.id, userId});
+  const { hasLiked, toggleLike } = useLike({ postId: data.id, userId});
 
   const goToUser = useCallback((ev: any) => {
     ev.stopPropagation();
@@ -36,10 +36,10 @@ const PostItem: React.FC<PostItemProps> = ({ data = {}, userId }) => {
       return loginModal.onOpen();
     }
 
-    //toggleLike();
-  }, [loginModal, currentUser]);
+    toggleLike();
+  }, [loginModal, currentUser, toggleLike]);
 
-  //const LikeIcon = hasLiked ? AiFillHeart : AiOutlineHeart;
+  const LikeIcon = hasLiked ? AiFillHeart : AiOutlineHeart;
 
   const createdAt = useMemo(() => {
     if (!data?.createdAt) {
@@ -121,7 +121,7 @@ const PostItem: React.FC<PostItemProps> = ({ data = {}, userId }) => {
                 transition 
                 hover:text-red-500
             ">
-              {/* <LikeIcon color={hasLiked ? 'red' : ''} size={20} /> */}
+              <LikeIcon color={hasLiked ? 'red' : ''} size={20} />
               <p>
                 {data.likedIds.length}
               </p>
